b29ac1116df2afb87e4e354142a7ecc79dbdc546
Malik Wali <defdotpy@gmail.com>
2022-06-17 19:23:01 +0700

    feat: load balancing config

---
+++ install-nginx.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+set -eE -o functrace
+
+failure() {
+    local statuscode=$? ; local lineno=$2 ; local msg=$3
+    echo "Failed with exit code ($statuscode) at line ($lineno) => $msg"
+}
+
+trap 'failure ${LINENO} "$BASH_COMMAND"' ERR
+
+mkdir -p $APPLICATION_DIR
+cd $APPLICATION_DIR || exit 2
+
+# Install NGINX
+sudo apt install -qq nginx
+sudo ufw allow 'Nginx Full'
+sudo systemctl restart nginx
+
